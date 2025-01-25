378dfa0408b5e2c5d19e0d6b7ee2f3f31885aa8e
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-24 21:15:49 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl">Tailwind CSS is Working!</h1>
+    </div>
+  )
+}
+
+export default App
